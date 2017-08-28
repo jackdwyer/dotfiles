fe1816f54d75613ccc120b4df62b2b23f103acfb
jackdwyer <jack.dwyer@blueapron.com>
2017-08-28 14:37:13 -0400

    removes static key, reduces font size

--- st/config.h
+++ st/config.h
@@ -5,8 +5,8 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Terminus:pixelsize=30:antialias=false:autohint=true";
-static int borderpx = 2;
+char font[] = "Terminus:pixelsize=22:antialias=false:autohint=true";
+int borderpx = 2;
 
 /*
  * What program is execed by st depends of these precedence rules:
