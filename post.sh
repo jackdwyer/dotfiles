d969d249e344a3cf9bf58427ef65385c123b8bd9
jackdwyer <jackjack.dwyer@gmail.com>
2019-11-29 23:53:47 +1100

    dont spam the internet

--- post.sh
+++ post.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 ${DOT_DIR}/tweet/tweet &
-${DOT_DIR}/bin/check_for_updates &
-${DOT_DIR}/bin/dotfile_modified &
+# ${DOT_DIR}/bin/check_for_updates &
+# ${DOT_DIR}/bin/dotfile_modified &
